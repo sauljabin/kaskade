50347def1b4c411b5fe0a7d71a53fc9833cab29c
Saúl Piña <sauljabin@gmail.com>
2024-07-15 11:15:07 -0500

    add typos to analyze script

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -6,6 +6,7 @@ def main():
         "checking types": "poetry run mypy kaskade/",
         "black": "poetry run black --check .",
         "ruff": "poetry run ruff check .",
+        "typos": "poetry run typos --format brief",
     }
     command_processor = CommandProcessor(commands)
     command_processor.run()
