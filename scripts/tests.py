99302378532ff9d295220589880f255da9881f10
Saúl Piña <sauljabin@gmail.com>
2024-11-17 12:40:43 -0500

    fix: script

--- scripts/tests.py
+++ scripts/tests.py
@@ -6,8 +6,9 @@ from scripts import CommandProcessor
 @click.command()
 @click.option("--e2e", "e2e", is_flag=True, help="Run e2e tests.")
 def main(e2e: bool) -> None:
+    module = "tests-e2e" if e2e else "tests"
     commands = {
-        "executing tests": f"poetry run python -m unittest discover -v {"tests-e2e" if e2e else "tests"}",
+        "executing tests": f"poetry run python -m unittest discover -v {module}",
     }
     command_processor = CommandProcessor(commands)
     command_processor.run()
